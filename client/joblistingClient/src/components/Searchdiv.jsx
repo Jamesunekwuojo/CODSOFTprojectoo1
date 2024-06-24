7e3939ddb09c640fa2b5c4dba0f4c707ea8d8af9
jamesunekwuojo <unekwuojoabah4@gmail.com>
2024-06-24 15:52:19 +0100

    react box added

--- client/joblistingClient/src/components/Searchdiv.jsx
+++ client/joblistingClient/src/components/Searchdiv.jsx
@@ -8,6 +8,7 @@ function Searchdiv() {
 
   const handleLocationSelect = (e) => {
     setSelectedLocation(e);
+    
   };
 
   const handleCategorySelect = (e) => {
--- client/joblistingClient/src/pages/Homepage.jsx
+++ client/joblistingClient/src/pages/Homepage.jsx
@@ -1,19 +1,37 @@
-import { Container, Row } from 'react-bootstrap';
-import Searchdiv from "../components/Searchdiv"
+import { Container, Row, Col } from 'react-bootstrap';
+import Searchdiv from "../components/Searchdiv";
 
 function Homepage() {
   return (
-    <Container className="d-flex justify-content-center align-items-center min-vh-100">
-      <Row className="justify-content-center">
-      <h1 className="text-center">+2000 Jobs Available</h1>
-        <h2 className="text-center">EXPLORE JOBHUB</h2>
-
-        <Searchdiv></Searchdiv>
+    <Container className="d-flex flex-column justify-content-center align-items-center min-vh-100">
+      <Row className="justify-content-center text-center mb-4">
+        <Col>
+          <h1>+2000 Jobs Available</h1>
+          <h2>EXPLORE JOBHUB</h2>
+        </Col>
       </Row>
 
-    
+      <Row className="justify-content-center mb-4">
+        <Col>
+          <Searchdiv />
+        </Col>
+      </Row>
 
+      <Row className="justify-content-center">
+        <Col className="d-flex justify-content-center">
+          <div className="box p-4 border rounded shadow-sm">Box 1</div>
+        </Col>
+        <Col className="d-flex justify-content-center">
+          <div className="box p-4 border rounded shadow-sm">Box 2</div>
+        </Col>
+        <Col className="d-flex justify-content-center">
+          <div className="box p-4 border rounded shadow-sm">Box 3</div>
+        </Col>
 
+        <Col className="d-flex justify-content-center">
+          <div className="box p-4 border rounded shadow-sm">Box 3</div>
+        </Col>
+      </Row>
     </Container>
   );
 }
