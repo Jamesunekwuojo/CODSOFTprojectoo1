4b7a6061f2aedb3e78afaf299432cecf4f6b928e
jamesunekwuojo <unekwuojoabah4@gmail.com>
2024-07-25 15:25:03 +0100

    choose us section layout gotten 60%

--- client/joblistingClient/src/components/ChooseUs.jsx
+++ client/joblistingClient/src/components/ChooseUs.jsx
@@ -1,32 +1,44 @@
+import React from 'react';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import "../App.css"; // Make sure to add your styles in this file
 
-import { Container, Row, Col } from 'react-bootstrap';
-import "../App.css"
-
-function ChooseUs ()   {
+function ChooseUs() {
   return (
-   <Container>
-  
-    <Row className='chooseUs'>
-
-      
-
-      <Col xs={12} md={8} className="chooseUstext">
-      <div className='textDiv'>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi quod iure eos eius id quisquam accusamus. Vel sint, provident quasi sequi blanditiis ut culpa ad beatae distinctio? Repellat aspernatur pariatur, cum dolore fugiat amet dicta delectus aliquid, odio aut hic.
-      </div>
-      
-
-      </Col>
-
-      <Col className='chooseUsimg'>
-      <div>
-        <img></img>
-      </div>
-
-      </Col>
-    </Row>
-
-   </Container>
+    <Container className="d-flex justify-content-center my-5">
+      <Row className="align-items-center">
+        {/* Left Column with Images */}
+        <Col xs={12} md={6} lg={6} className="d-flex flex-column">
+          <div className='chooseUsDiv mb-4'>
+            {/* Image or content for the first div */}
+            <img src="image1.jpg" alt="First" className="img-fluid" />
+          </div>
+          <div className='chooseUsDiv mb-4 ml-auto'>
+            {/* Image or content for the second div */}
+            <img src="image2.jpg" alt="Second" className="img-fluid" />
+          </div>
+          <div className='chooseUsDiv'>
+            {/* Image or content for the third div */}
+            <img src="image3.jpg" alt="Third" className="img-fluid" />
+          </div>
+        </Col>
+        {/* Right Column with Text */}
+        <Col xs={12} md={6} lg={6} className="chooseUstext d-flex flex-column justify-content-center">
+          <div className='textDiv'>
+            <h2>Get the job of your dreams quick & easy.</h2>
+            <p>
+              Search all the open positions on the web. Get your own personalized salary estimate.
+              Read reviews on over 30000+ companies worldwide.
+            </p>
+            <ul>
+              <li>Digital Marketing Solutions for Tomorrow</li>
+              <li>Our Talented & Experienced Marketing Agency</li>
+              <li>Create your own skin to match your brand</li>
+            </ul>
+            <Button variant="success">Contact us</Button>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
