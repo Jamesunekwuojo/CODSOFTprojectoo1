b910dd350bc57f5e241cb2966c113f2f15ba96c4
jamesunekwuojo <unekwuojoabah4@gmail.com>
2024-08-25 13:26:14 +0100

    protected route completed

--- client/joblistingClient/src/pages/ProtectedRoute.jsx
+++ client/joblistingClient/src/pages/ProtectedRoute.jsx
@@ -1,12 +1,26 @@
 // src/pages/ProtectedRoute.jsx
-import { Navigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 const ProtectedRoute = ({ children }) => {
   const isAuthenticated = localStorage.getItem('token'); // Replace with your actual authentication check
+  const navigate = useNavigate();
 
-  if(isAuthenticated)
+  useEffect(() => {
+    if (!isAuthenticated) {
+      Swal.fire({
+        title: "Warning",
+        text: "You must be logged in to post job, sign in to continue ",
+        icon: "warning",
+        confirmButtonText: "OK"
+      }).then(() => {
+        navigate('/signin', { replace: true });
+      });
+    }
+  }, [isAuthenticated, navigate]);
 
-  return isAuthenticated ? children : <Navigate to="/signin" />;
+  return isAuthenticated ? children : null; 
 };
 
 export default ProtectedRoute;
