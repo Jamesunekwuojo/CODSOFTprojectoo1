34907c39918f4c384b83f3852285d2ad991f6ba8
jamesunekwuojo <unekwuojoabah4@gmail.com>
2024-09-03 11:43:08 +0100

    working on the form input for complete profile section

--- client/joblistingClient/src/components/CompleteProfile/CompleteProfile.jsx
+++ client/joblistingClient/src/components/CompleteProfile/CompleteProfile.jsx
@@ -5,6 +5,15 @@ function CompleteProfile ()  {
         <div>
             <Form>
                 <h1>Complete Profile</h1>
+                <Form.Group>
+
+                <Form.Label>
+                    Upload ur profile picture
+                </Form.Label>
+                <Form.Control></Form.Control>
+
+                </Form.Group>
+                
             </Form>
 
         </div>
