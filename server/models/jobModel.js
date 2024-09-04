3378f0458b1555db0d44e31ed394d85fe8b7e823
jamesunekwuojo <unekwuojoabah4@gmail.com>
2024-09-04 09:35:33 +0100

    creating routes for job

--- server/models/jobModel.js
+++ server/models/jobModel.js
@@ -1,5 +1,16 @@
 import mongoose from "mongoose";
 
 const jobSchema = new mongoose.Schema({
+    JobTitle: {type:String},
+    JobLocation : {type:String},
+    JobType: {type:String},
+    MinimumSalary: {type:String},
+    MaximumSalary :{},
+    ApplicationDeadline: {},
+    JobDescription: {},
     
-})
+});
+
+const  JobModel = mongoose.model("job", jobSchema);
+
+export default JobModel;
---
+++ server/routes/job.js
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.post('/createjob',);
+router.get('/displaybyId');
+router.get('/dispalyJob',)
+
+export default router
