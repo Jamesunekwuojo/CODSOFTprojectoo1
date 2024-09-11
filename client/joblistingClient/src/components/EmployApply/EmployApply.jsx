a8e94b02f49842b07b322f3b4cd471e78717428a
jamesunekwuojo <unekwuojoabah4@gmail.com>
2024-09-11 10:46:47 +0100

    some edits made to make form widths responsive across devices

--- client/joblistingClient/src/components/EmployApply/EmployApply.jsx
+++ client/joblistingClient/src/components/EmployApply/EmployApply.jsx
@@ -1,28 +1,30 @@
 import { Container, Form, Row, Col, Button } from "react-bootstrap";
 const EmployApply = () => {
 
-    const handleSubmit = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
 
     }
   return (
-    <Container className="d-flex justify-content-center"> 
-      <Row>
-        <Col xs={12}>
-          <Form className="border p-4 border-secondary-subtle" onSubmit={handleSubmit}>
-            <div>
+    <Container className="d-flex justify-content-center  vh-90"> 
+      <Row className="w-100 justify-content-center">
+        <Col xs={12} md={8} lg={10} >
+          <Form className=" border p-4 rounded-5 mt-4 border-secondary-subtle " onSubmit={handleSubmit}>
+            <div className="d-flex justify-content-center">
               <h2>Hi Dear, what do you like to do?</h2>
             </div>
 
             <Form.Group controlId="formBasicEmail">
               <Form.Control as="select" name="role">
-                <option>Select ...</option>
-                <option value="employ"></option>
-                <option value="apply for jobs"></option>
+                <option>Select from options below ...</option>
+                <option value="employ">Employ</option>
+                <option value="apply for jobs">Apply for job</option>
               </Form.Control>
             </Form.Group>
 
-            <Form.Group>
-                <Button type="submit"></Button>
+            <Form.Group className="p-4 d-flex justify-content-center">
+                <Button type="submit">Enter </Button>
             </Form.Group>
           </Form>
         </Col>
