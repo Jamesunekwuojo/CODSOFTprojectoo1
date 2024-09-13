4171d84ea8dce54fa0cfa84b75836d28afc3f40c
jamesunekwuojo <unekwuojoabah4@gmail.com>
2024-09-13 08:40:35 +0100

    about us image added

--- client/joblistingClient/src/components/AboutUs/AboutUs.jsx
+++ client/joblistingClient/src/components/AboutUs/AboutUs.jsx
@@ -1,10 +1,33 @@
+import { Container, Row, Col } from "react-bootstrap"
 
 
 const AboutUs = () => {
     return (
-        <div>
-            <h1>Hello</h1>
-        </div>
+        <Container className="d-flex justify-content-center flex-column">
+            <Row>
+                <Col>
+                <header className="d-flex justify-content-center">
+                    <h1>About us</h1>
+                </header>
+                
+                </Col>
+            </Row>
+            <Row>
+                <Col className="d-flex justify-content-center">
+                <img src="/aboutus.png"/>
+                
+
+                </Col>
+            </Row>
+            <Row className="">
+                <Col className="d-flex justify-content-center">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut aliquid itaque consequatur quo perspiciatis fuga impedit. Quod optio illum id incidunt facere libero sit quis inventore, deleniti nisi repellendus nemo.</p>
+                </Col>
+
+            </Row>
+
+        
+        </Container>
     )
 
 }
