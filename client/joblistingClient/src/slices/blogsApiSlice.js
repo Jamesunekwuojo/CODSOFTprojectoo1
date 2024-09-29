2e2f06ac657412fc160ecd27d53fdc2bd883da2c
jamesunekwuojo <unekwuojoabah4@gmail.com>
2024-09-29 17:12:29 +0100

    modifying blogs api Slice content

--- client/joblistingClient/src/slices/blogsApiSlice.js
+++ client/joblistingClient/src/slices/blogsApiSlice.js
@@ -1,3 +1,35 @@
 import {apiSlice} from "./apiSlice.js";
 
-e
+export const blogsApiSlice = apiSlice.injectEndpoints({
+    endpoints: (builder) => ({
+
+        createBlog: builder.mutation({
+            query: (formData) => ({
+                url: '/api/createblog',
+                method:'POST',
+                body: formData
+            })
+        }),
+
+        // query to get specific employer blog
+
+        getEmployerBlogs: builder.query({
+
+            query: () => ({
+                url: '/api/blogsbyId',
+                method: 'GET',
+
+            })
+
+        }),
+
+    })
+   
+})
+
+
+export const {
+    useCreateBlogMutation,
+    useGetEmployerBlogsQuery,
+
+} = blogsApiSlice
