565e3e0ad1d0833f0d04c1e047f0fe758ffd7879
jamesunekwuojo <unekwuojoabah4@gmail.com>
2024-10-28 03:40:47 +0100

    mongo duplicate error fixed

--- server/utilis/utilis.js
+++ server/utilis/utilis.js
@@ -1,10 +1,10 @@
 import jwt from 'jsonwebtoken';
 
 
-const JWTkey = process.env.JWT_key;
+const JWTkey = process.env.JWT_KEY;
 
 // export const createToken = (_id) => {
-//     return jwt.sign({_id},  JWTkey, {expiresIn:'12h'})
+//     return jwt.sign({_id},  JWTkey, {epiresIn:'12h'})
 // }
 
 
