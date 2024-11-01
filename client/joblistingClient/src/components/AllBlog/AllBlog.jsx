eaea5c6af1016417c827a9a91264fa8b1c4aaa45
jamesunekwuojo <unekwuojoabah4@gmail.com>
2024-11-01 15:20:59 +0100

    blog circular  image effect achieved

--- client/joblistingClient/src/components/AllBlog/AllBlog.jsx
+++ client/joblistingClient/src/components/AllBlog/AllBlog.jsx
@@ -1,12 +1,9 @@
-// import { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import {Container, Row, Col, Card, Button} from "react-bootstrap";
 import { useGetAllBlogsQuery} from "../../slices/blogsApiSlice";
 import { toast } from "react-toastify";
 
 import "./AllBlog.css"
-
-
-
 
 const AllBlog =() =>{
 
@@ -20,22 +17,25 @@ const AllBlog =() =>{
 
   }
 
-  if (blogs.Blogs.length ==0) {
-    return <p> No Blog found found for this employer</p>
+  if (blogs.Blogs.length == 0) {
+    return <p> No Blog found for this employer</p>
   }
 
-
-  
-
-
-  return(
+  return (
     <Container>
       <h2 className="text-center"> Popular Blogs Available</h2>
       <Row>
-       {blogs.Blogs.map(blog => (
+        {blogs.Blogs.map(blog => (
           <Col md={4} key={blog._id} className="mb-4">
-            <Card className="job-card ">
-              <Card.Img variant="top" src={blog.profilePhoto.url} alt={blog.profilePhoto.filename} />
+            <Card className="job-card">
+              <div className="image-container">
+                <Card.Img
+                  variant="top"
+                  src={blog.profilePhoto.url}
+                  alt={blog.profilePhoto.filename}
+                  className="circular-image"
+                />
+              </div>
               <Card.Body className="job-card-body">
                 <Card.Title>{blog.articleTitle}</Card.Title>
                 <Card.Text>{blog.articleDescript}</Card.Text>
@@ -47,12 +47,9 @@ const AllBlog =() =>{
             </Card>
           </Col>
         ))}
-
-        
       </Row>
     </Container>
   );
 }
+
 export default AllBlog;
-
-
