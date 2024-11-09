4e0538399343360fddc6ac26f7c34348cac4e157
jamesunekwuojo <unekwuojoabah4@gmail.com>
2024-11-09 14:20:42 +0100

    updated blog route file

--- server/routes/blog.js
+++ server/routes/blog.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { uploadProfilePhoto, CreateBlog, GetEmployerblogs, GetBlogs  } from "../controllers/blogController.js";
+import { uploadProfilePhoto, CreateBlog, GetEmployerblogs, GetBlogs, UpdateBlog, DeleteBlog  } from "../controllers/blogController.js";
 
 import { protectAuth } from "../middleware/authMiddleware.js";
 
@@ -12,6 +12,11 @@ const router = express.Router();
 // router.use(protectAuth);
 
 router.get('/allBlogs', GetBlogs);
+
+router.put('/api/blog/:id', uploadProfilePhoto, UpdateBlog);
+
+router.delete('/api/blog/:id', DeleteBlog);
+
 router.post( '/createblog', protectAuth, uploadProfilePhoto,   CreateBlog );
 
 router.get('/blogs:byId', protectAuth, GetEmployerblogs );
