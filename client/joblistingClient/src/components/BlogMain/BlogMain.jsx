5dec52b6ff9d6302946b29a82b91fdac46a191d0
Jamesunekwuojo <unekwuojoabah4@gmail.com>
2024-12-30 18:10:43 +0100

    blog main folder creted

---
+++ client/joblistingClient/src/components/BlogMain/BlogMain.jsx
@@ -0,0 +1,16 @@
+import { Container, Row, Col } from "react-bootstrap"
+
+const BlogMain = () => {
+
+    return (
+        <Container>
+            <Row>
+                <Col></Col>
+            </Row>
+        </Container>
+
+    )
+
+}
+
+export  default BlogMain
