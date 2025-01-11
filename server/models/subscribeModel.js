d26128403e2b6eeaeaa7292c7fd18c94487c0a8c
Jamesunekwuojo <unekwuojoabah4@gmail.com>
2025-01-11 10:40:28 +0100

    working on subscribe model functionality

--- server/models/subscribeModel.js
+++ server/models/subscribeModel.js
@@ -4,5 +4,18 @@ import mongoose from "mongoose";
 const subscribeSchema = new mongoose.Schema({
     email: { 
         type: String,
-    }
-})
+        unique:true,
+        required: true,
+
+        match: [/\S+@\S+\.\S+/, 'Invalid email address']
+    },
+
+    subscribeAt: {
+        type:Date,
+        default:Date.now(),
+    },
+});
+
+const subscribeModel = mongoose.model('Subscriber', subscribeSchema);
+
+export {subscribeModel as Subscriber}
