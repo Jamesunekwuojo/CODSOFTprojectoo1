03f23de0c32db2c163282c34ce981e9c3394303d
Jamesunekwuojo <unekwuojoabah4@gmail.com>
2025-01-16 07:21:06 +0100

    latest blog words truncation for read more enabled

--- client/joblistingClient/src/components/Latestblogs/Latestblogs.jsx
+++ client/joblistingClient/src/components/Latestblogs/Latestblogs.jsx
@@ -3,62 +3,80 @@ import { Container, Row, Col, Card, Button } from "react-bootstrap";
 // import { useGetAllBlogsQuery } from "../../slices/blogsApiSlice";
 import { useLatestBlogsQuery } from "../../slices/blogsApiSlice";
 import { toast } from "react-toastify";
+import { Link } from "react-router-dom";
 
 const Latestblogs = () => {
-    const { data: blogs, isLoading, error } = useLatestBlogsQuery();
+  const { data: blogs, isLoading, error } = useLatestBlogsQuery();
 
-    if(isLoading) return <p>Loading...</p>
+  if (isLoading) return <p>Loading...</p>;
 
-    if(error) {
-        // toast.error("Error fetching latest blogs");
-        console.log(error?.data?.error || "Error fetching latest blogs")
+  if (error) {
+    // toast.error("Error fetching latest blogs");
+    console.log(error?.data?.error || "Error fetching latest blogs");
 
-        return <p>Failed to fetch latest blogs...</p>
+    return <p>Failed to fetch latest blogs...</p>;
+  }
+
+  const truncateBlog = (bodyText, wordLimit) => {
+    const words = bodyText.split(" ");
+
+    if (words.length > wordLimit) {
+      return { truncated: words.slice(0, wordLimit).join(" "), full: true };
     }
+    return { truncated: bodyText, full: false };
+  };
 
-    const truncateBlog = (bodyText, wordLimit) => {
-      const words = bodyText.split("");
-
-      if(words.length > wordLimit){
-        return {truncated: words.slice(0, wordLimit)}
-      }
-    }
-
-
-
-  return ( 
+  return (
     <Container>
       <h2 className="text-center">Latest Blogs</h2>
       <Row>
-        {blogs.Blogs.map((blog) => (
-          <Col md={4} key={blog._id} className="mb-4">
-            <Card className="job-card" style={{ width: '18rem', height: '24rem' }}>
-              <div className="image-container" style={{ height: '150px', overflow: 'hidden' }}>
-                <Card.Img
-                  variant="top"
-                  src={blog.profilePhoto.url}
-                  alt={blog.profilePhoto.filename}
-                  className="circular-image"
-                  // style={{ width: '100%', height: '100%', objectFit: 'cover' }}
-                />
-              </div>
-              <Card.Body className="job-card-body" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
-                <Card.Title>{blog.articleTitle}</Card.Title>
-                <Card.Text>{blog.articleDescript}</Card.Text>
-                <Button variant="primary" href={blog.articleLink} target="_blank">
-                  Read More
-                </Button>
-              </Card.Body>
-              <Card.Footer>
-                <small className="text-muted">
-                  By {blog.authorName} | {blog.authorEmail}
-                </small>
-              </Card.Footer>
-            </Card>
-          </Col>
-        ))}
+        {blogs.Blogs.map((blog) => {
+          const { truncated, full } = truncateBlog(blog.articleDescript, 6);
+
+          return (
+            <Col md={4} key={blog._id} className="mb-4 ">
+              <Card style={{ width: "17rem", height: "24rem" }}>
+                <div style={{ height: "150px", overflow: "hidden" }}>
+                  <Card.Img
+                    variant="top"
+                    src={blog.profilePhoto.url}
+                    alt={blog.profilePhoto.filename}
+                    style={{
+                      width: "100%",
+                      height: "100%",
+                      objectFit: "cover",
+                    }}
+                  />
+                </div>
+
+                <Card.Body
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  <Card.Title>{blog.articleTitle}</Card.Title>
+
+                  <Card.Text>
+                    {truncated}
+                    {full && (
+                      <Link className="mx-4" to={`/blog/${blog._id}`}>
+                        Read more
+                      </Link>
+                    )}
+                  </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                  <small className="text-muted">
+                    By {blog.authorName} | {blog.authorEmail}
+                  </small>
+                </Card.Footer>
+              </Card>
+            </Col>
+          );
+        })}
       </Row>
-     
     </Container>
   );
 };
